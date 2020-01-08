b6e38b2036b5953e294ced90f891be7f1084350f
bblakely <bethany.j.blakely@gmail.com>
2020-01-08 16:31:45 -0600

    Several additions to AddInfo as more data become available. Added GPS, lodging score, and yield. Also changed how flood flag is added;instead of doing this after the fact it's merged in (albeit inefficiently) with the other ancillary datasets. Also a few adaptations for directory restructuring.

--- AddInfo_LP.R
+++ AddInfo_LP.R
@@ -15,14 +15,28 @@ plotmeans$raw.order<-c(as.numeric(rownames(plotmeans)))
 #####
 
 #Read in ancillary datasets and format for merging####
-
+dir.main<-getwd()
+setwd('Ancillary')
 info.raw<-read.csv('PlotInfo_2019.csv')
 count.raw<-read.csv('StandCount_2019.csv')
 heights.raw <- read.csv('LidarHeight_2019.csv', skip=2, stringsAsFactors = FALSE)
 lai.raw<-read.csv('LidarLAI_2019.csv', skip=2, stringsAsFactors = FALSE)
 yield.raw<-read.csv('Yield_2019.csv')
+alt.raw<-read.csv('GPS_2019.csv')
+lodge.raw<-read.csv("Lodging_2019.csv")
+setwd(dir.main)
 
-#Unpack McGrath style dates and subset heights, LAI to date closest to LP measurement date
+#Read in albedo dataset (will take a long time the first time)
+if(!exists('vis.400.700')){source('Readin_SR.R')}
+dat.sr->sr.raw
+colnames(sr.raw)[colnames(sr.raw)=="dectime"]<-'dectime.sr' #Rename dectime column to prevent conflict with lp dectime column (and resulting unwanted renaming) when merging
+
+#Read in and process flood dataset
+flood.raw<-read.csv("Ancillary/FloodPlots_2019.csv")
+flood.raw$Edge<-as.character(flood.raw$Edge); flood.raw$Edge[flood.raw$Edge=="y"|flood.raw$Edge=='n']<-1
+
+
+#Unpack McGrath style dates and subset heights, LAI to date closest to LP measurement date####
 subdate<-function(dat, doy, year=2019, datecol='date'){ #Function may not work with different date formats!
   dat.ts<-as.POSIXct(dat[,which(colnames(dat)==datecol)])
   dat$YEAR<-as.numeric(format(dat.ts, format= '%Y'))
@@ -46,24 +60,31 @@ light$exp.order<-c(1:nrow(light)) #Record thinned order before merges, which scr
 
 #####
 
-##Merge in stand counts, plot ID's, heights, LAI's####
+##Merge in stand counts, plot ID's, heights, LAI's, etc. ####
 
+#This made sense when I was merging just 2-3 things together but this is getting ridiculous...
 light1<-merge(light, info.raw, by.x=c('row', 'range'),by.y=c('first_row','range'), sort=FALSE) #Add plot info
 light2<-merge(light1, count.raw,by.x=c('row', 'range'),by.y=c('first_row','range'), sort=FALSE) #Add stand counts
 light3<-merge(light2, heights.lp, by.x=c('row', 'range'), by.y=c('first_row', 'range'),all.x=TRUE, sort=FALSE) #Add heights
 light4<-merge(light3, lai.lp, by.x=c('row', 'range'), by.y=c('first_row', 'range'),all.x=TRUE, sort=FALSE) #Add LAI
 light5<-merge(light4, yield.raw, by.x=c('row', 'range'), by.y=c('Row', 'Range'),all.x=TRUE, sort=FALSE) #Add yield
+light6<-merge(light5, lodge.raw, by.x=c('row', 'range'), by.y=c('Row', 'Range'),all.x=TRUE, sort=FALSE) #Add lodging score
+light7<-merge(light6, alt.raw, by.x=c('row', 'range'), by.y=c('row', 'range'),all.x=TRUE, sort=FALSE) #Add GPS
+light8<-merge(light7, sr.raw, by.x=c('row', 'range'), by.y=c('row', 'range'),all.x=TRUE, sort=FALSE) #Add albedo
 
+light9<-merge(light8, y=flood.raw, by.x=c('row', 'range'), by.y=c('RowStart', 'Range'), all.x=TRUE, sort=FALSE)#Add flood index
+light9<-light9[order(light9$exp.order),];light9$Edge[is.na(light9$Edge)]<-0 #requires reordering, setting NAs to 0
 #####
 
 #Clear out junk columns; creates 'dat.lp' ####
 
-light.full<-light5
+light.full<-light9
 colnames(light.full)
 
 var.want<-c("row", "range", "dectime", "DOY.x", "H", "M","S",
             "PPF1_Avg", "PPF2_Avg", "PPF3_Avg", "PPF4_Avg", "PPF5_Avg","PPF_above_Avg",
-            "noisy","ns","raw.order","exp.order","plot_id","genotype_name","block_id", "set_id","row_density","height","lai", "Sum_lbs")
+            "noisy","ns","raw.order","exp.order","plot_id","genotype_name","block_id", "set_id",
+            "row_density","height","lai", "Sum_lbs","x", "y", "z","Score", "vis.400.700", "nir.700.1000", "Edge")
   
   #Full variable list
   #c("row", "range", "dectime", "DOY.x", "H", "M","S","BattV_Min",
@@ -75,12 +96,12 @@ cols<-which(colnames(light.full)%in%var.want); colnames(light.full)[cols] #Colum
 dat.lp<-light.full[,cols]
 
 #####
-
-#Grab checkline plots; creates dat.check #####
-library(stringr)
-checkind<-str_detect(dat.lp$set_id, "CHK")
-dat.check<-dat.lp[str_detect(dat.lp$set_id, "CHK"),]
-#####
+# 
+# #Grab checkline plots; creates dat.check #####
+# library(stringr)
+# checkind<-str_detect(dat.lp$set_id, "CHK")
+# dat.check<-dat.lp[str_detect(dat.lp$set_id, "CHK"),]
+# #####
 
 
 
@@ -108,17 +129,26 @@ if(mod.toc=="TRUE"){
 
 #Acknowledge flood zone
 
-floodlist<-read.csv("FloodPlots_2019.csv")
-dat.lp$order<-c(1:nrow(dat.lp))
-dat.flood<-data.frame(merge(x=dat.lp, y=floodlist, by.x=c('row', 'range'), by.y=c('RowStart', 'Range'), all.x=TRUE, sort=FALSE))
-dat.flood$Edge.<-as.character(dat.flood$Edge.); dat.flood$Edge.[dat.flood$Edge.=="y"|dat.flood$Edge.=='n']<-1
-dat.flood<-dat.flood[order(dat.flood$order),] #reorder to match dat.re and have metadata at the front where I can see it
-dat.flood$Edge.[is.na(dat.flood$Edge.)]<-0
+# floodlist<-read.csv("Ancillary/FloodPlots_2019.csv")
+# dat.lp$order<-c(1:nrow(dat.lp))
+# dat.flood<-data.frame(merge(x=dat.lp, y=floodlist, by.x=c('row', 'range'), by.y=c('RowStart', 'Range'), all.x=TRUE, sort=FALSE))
+# dat.flood$Edge.<-as.character(dat.flood$Edge.); dat.flood$Edge.[dat.flood$Edge.=="y"|dat.flood$Edge.=='n']<-1
+# dat.flood<-dat.flood[order(dat.flood$order),] #reorder to match dat.re and have metadata at the front where I can see it
+# dat.flood$Edge.[is.na(dat.flood$Edge.)]<-0
 
 
+#Grab checkline plots; creates dat.check #####
+library(stringr)
+checkind<-str_detect(dat.lp$set_id, "CHK")
+dat.check<-dat.lp[str_detect(dat.lp$set_id, "CHK"),]
+#####
 
 
 #CLEANUP
-rm('lp.raw','lp.ts','light1','light2','light3','light4', 'light5','light','plot.cl','splits','heights.lp','lai.lp', 'light.full', 'timestamp.col', 'split.dir', 'doy.of.interest', 'modtop','newtops', 'rowtops') #intermediate steps in processing
-rm('count.raw', 'info.raw', 'heights.raw','lai.raw', 'floodlist', 'yield.raw') #Raw ancillary
+rm('lp.raw','lp.ts','light1','light2','light3','light4', 'light5','light6', 'light7','light8','light9','light','plot.cl','splits','heights.lp','lai.lp', 'light.full', 'timestamp.col', 'split.dir', 'doy.of.interest', 'modtop','newtops', 'rowtops', 'dat.sr') #intermediate steps in processing
+rm('count.raw', 'info.raw', 'heights.raw','lai.raw', 'yield.raw', 'lodge.raw', 'alt.raw', 'sr.raw', 'flood.raw') #Raw ancillary
+
+rm('dat.lp.orig') #Comment this out if you want uncorrected top-of-canopy light levels
+rm('plotmeans','refl')#Comment this out if you want border plots and spectral reflectances
+
 
