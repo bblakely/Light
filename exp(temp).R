887888be571bd1877122e5508cf24402be50a15c
bblakely <bethany.j.blakely@gmail.com>
2020-02-11 13:57:42 -0600

    Added half-canopy function

--- exp(temp).R
+++ exp(temp).R
@@ -72,21 +72,59 @@ library(ggplot2)
 
 which(dat.flood$z==max(dat.flood$z[dat.flood$Edge.==1]))
 
-dat.flood$Z[319]<-NA
+# dat.flood$Z[319]<-NA
+# 
+# ggplot(dat.flood) +
+#  aes(x = z, y = Sum_lbs, colour = Edge.) +
+#  geom_point(size = 1.78) +
+#  geom_smooth(span = 1L, method='lm') +
+#  scale_color_brewer(palette = "Dark2") +
+#  labs(x = "Altitude", y = "Yield", color = "Flood") +
+#  theme_minimal()
+# 
+# 
+# summary(lm(dat.flood$Sum_lbs[dat.flood$Edge.==1]~dat.flood$z[dat.flood$Edge.==1]))
+# 
+# ggplot(dat.lp) +
+#   aes(x = row, y = range, fill = z) +
+#   geom_tile(size = 1L) +
+#   scale_fill_distiller(palette = "Spectral") +
+#   theme_minimal()
 
-ggplot(dat.flood) +
- aes(x = z, y = Sum_lbs, colour = Edge.) +
- geom_point(size = 1.78) +
- geom_smooth(span = 1L, method='lm') +
- scale_color_brewer(palette = "Dark2") +
- labs(x = "Altitude", y = "Yield", color = "Flood") +
- theme_minimal()
 
 
-summary(lm(dat.flood$Sum_lbs[dat.flood$Edge.==1]~dat.flood$z[dat.flood$Edge.==1]))
 
-ggplot(dat.lp) +
-  aes(x = row, y = range, fill = z) +
+#Run normalizeLP before this
+
+
+extract.canval<-function(dat.h, dat.l, pct=0.5, res=20){
+  holder<-rep(-1, nrow(dat.l))
+  
+  for(i in 1:nrow(dat.l)){
+    
+    heightstr<-approx(as.numeric(dat.h[i,]), n=res)$y
+    datstr<-approx(as.numeric(dat.l[i,]), n=res)$y
+    
+    ind<-which(abs(heightstr-pct)==min(abs(heightstr-pct)))
+    at.pct<-datstr[ind]
+    
+    holder[i]<-at.pct
+  }
+  
+  return(holder)
+}
+
+pcts<-extract.canval(dat.h=height.rel, dat.l=dat.rel, res=100)
+
+plotdat.ph<-cbind(dat.lp, pcts)
+
+#library(esquisse)
+#esquisser()
+
+library(ggplot2)
+ggplot(plotdat.ph) +
+  aes(x = row, y = range, fill = pcts) +
   geom_tile(size = 1L) +
-  scale_fill_distiller(palette = "Spectral") +
+  scale_fill_gradientn(colours=c("#3A4919","#7B883F","Lightgoldenrod1","white")) +
   theme_minimal()
+
